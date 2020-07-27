fe42895323a928fb0d673b50b63c35d6f69b139e
christopher2202 <christoprz22@gmail.com>
2020-07-27 18:04:11 -0500

    esta es la primra vercion

---
+++ application/controllers/Prueba.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+use chriskacerguis\RestServer\RestController;
+require APPPATH.'/libraries/RestController.php';
+class Prueba extends RestController{
+
+	public function index_get(){
+		$this->response("hola", RestController::HTTP_OK);
+	}
+}
